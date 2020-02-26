9dc2801ce1ceef67e6cd365c317b958e1ef22e22
daan <2087884@talnet.nl>
2020-02-26 14:09:23 +0100

    het werkt

--- wisselgeld.php
+++ wisselgeld.php
@@ -2,36 +2,60 @@
 
 $input = $argv[1];
 $bedrag = floatval($input);
-
-define(
-    "GELDEENHEDEN", 
-    [
-    500,
-    200,
-    100,
-    50,
-    20,
-    10,
-    5,
-    2,
-    1
-    ]
-);   
-
-define(
-    "CENTEN",
-    [
-    50,
-    20,
-    10,
-    5
-    ]
-);
-
 $restbedrag = $bedrag;
 
+check($restbedrag);
+function check($restbedrag){
+    echo !is_numeric($restbedrag);
+    try{
+
+        
+        if( !is_numeric($restbedrag) || $restbedrag == "" || $restbedrag == null) {
+            throw new Exception("Je hebt geen bedrag meegegeven dat omgewisseld kan worden");
+        }
+                    if($restbedrag <= 0){
+                        throw new Exception("ik kan geen negatief bedrag wisselen");
+                    }
+        $restbedrag = euros($restbedrag) * 100;
+        cents($restbedrag);
+}
+catch(Exception $e){
+    echo $e->getMessage();
+}
+}
+
+
+
+  
+
+
+
+
 function euros($restbedrag){
+    define(
+        "GELDEENHEDEN", 
+        [
+        500,
+        200,
+        100,
+        50,
+        20,
+        10,
+        5,
+        2,
+        1
+        ]
+    ); 
     foreach(GELDEENHEDEN as $euro){
+        define(
+            "CENTEN",
+            [
+            50,
+            20,
+            10,
+            5
+            ]
+        );
         if($restbedrag>=$euro) {
             $aantalKeerEuroInRestBedrag = floor($restbedrag / $euro);
             $restbedrag = $restbedrag - $euro * $aantalKeerEuroInRestBedrag;
@@ -41,7 +65,6 @@ function euros($restbedrag){
     return $restbedrag;
 }
 
-$restbedrag = euros($restbedrag) * 100;
 
 function cents($restbedrag){
     foreach(CENTEN as $euro){
@@ -53,5 +76,4 @@ function cents($restbedrag){
     }
 }
 
-cents($restbedrag);
 ?>
